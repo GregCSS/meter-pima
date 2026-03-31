916699d7a60288d14538413ea25eb5b824d81046
dotping-me <dotping.hpg@gmail.com>
2026-03-31 20:33:10 +0400

    Started Order, OrderItem and MenuItem

---
+++ Order.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<OrderItem> orderItems;
+    private Payment linkedPayingCustomerPayment; // Can be Cash or Card
+
+    public Order(Payment linkedPayingCustomerPayment) {
+        this.orderItems = new ArrayList<OrderItem>();
+        this.linkedPayingCustomerPayment = linkedPayingCustomerPayment;
+    }
+
+    public OrderItem getOrder(int index) {
+        return this.orderItems.get(index);
+    }
+
+    // Quite a long name I know but I hope it makes sense
+    // It is basically a referrence to the customer the order is for
+    public Payment getLinkedPayingCustomerPayment() {
+        return this.linkedPayingCustomerPayment;
+    }
+
+    // Composition as OrderItem cannot exist if the whole Order is deleted
+    public void addOrderItem(MenuItem item, int quantity) {
+        this.orderItems.add(new OrderItem(item, quantity));
+    }
+}
---
+++ OrderItem.java
@@ -0,0 +1,18 @@
+public class OrderItem {
+    private MenuItem item;
+    private int quantity;
+
+    // Aggregation here because menu item exists independently of OrderItem
+    public OrderItem(MenuItem item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getItem() {
+        return this.item;
+    }
+
+    public int quantity() {
+        return this.quantity;
+    }
+}
